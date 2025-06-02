95340649952244c284cf855e0b078e1f1f0572d6
eacosta <eacosta0411@gmail.com>
2025-06-01 21:24:00 -0400

    Refactor sumAll function to handle cases where num1 is greater than num2

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,8 +1,15 @@
 const sumAll = function(num1, num2) {
     sumArray = [];
-    for ( i = num1; i <= num2; i++) {
+    if (num1 < num2) {
+        for ( i = num1; i <= num2; i++) {
         sumArray.push(i);
-    };
+        };
+    } else if (num1 > num2) {
+        for ( i = num2; i <= num1; i++) {
+        sumArray.push(i);
+        };        
+    }
+
     return sumArray.reduce(( total, current) => total + current, 0);
 };
 
