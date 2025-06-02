3a2e156a0257c0cef0195b07cdb15cb7efafc571
eacosta <eacosta0411@gmail.com>
2025-06-01 21:38:45 -0400

    Add input validation to sumAll function to handle non-numeric and negative values

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
 const sumAll = function(num1, num2) {
     sumArray = [];
-    if (num1 < num2) {
+    if (typeof(num1) != 'number' ||  typeof(num2) != 'number') {
+        return "ERROR"
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (num1 % 1 != 0 || !num2 % 1 != 0) {
+        return "ERROR"
+    } else if (num1 < num2) {
         for ( i = num1; i <= num2; i++) {
         sumArray.push(i);
         };
