c03f47e71c6f3abdbf5deae766c64589b6095d97
eacosta <eacosta0411@gmail.com>
2025-06-01 21:16:14 -0400

    Add num1 thru num2 into sumArray and then add all number in array

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,8 +1,9 @@
 const sumAll = function(num1, num2) {
     sumArray = [];
     for ( i = num1; i <= num2; i++) {
-
-    }
+        sumArray.push(i);
+    };
+    return sumArray.reduce(( total, current) => total + current, 0);
 };
 
 // Do not edit below this line
