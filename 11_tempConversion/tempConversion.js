5724d3e1baa3dfe03b5dc1996bfbc83a65fcd71c
eacosta <eacosta0411@gmail.com>
2025-06-02 21:26:20 -0400

    Fix return value formatting in temperature conversion functions

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(num) {
   result = ( num - 32 ) * 5/9;
-  return result.toFixed(1);
+  return +(result.toFixed(1));
 };
 
 const convertToFahrenheit = function(num) {
   result = num * 9/5 + 32;
-  return result.toFixed(1);
+  return +(result.toFixed(1));
 };
 
 // Do not edit below this line
