4c33620bd909e22fe097e9c458453ee555e3a900
eacosta <eacosta0411@gmail.com>
2025-07-19 17:42:23 -0400

    Passed all calculator tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
 const add = function(num1, num2) {
-  return num1 + num2
+  return num1 + num2;
 };
 
 const subtract = function(num1, num2) {
-	return num1 - num2
+	return num1 - num2;
 };
 
 const sum = function(arr) {
-	return arr.reduce( (sum, current) => sum + current, 0)
+	return arr.reduce( (sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (val, current) => val * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0){
+    return 1
+  };
+  let facto = [];
+  while ( a > 0) {
+    facto.push(a);
+    a--;
+  };
+  return facto.reduce( (val, current) => val * current);
 };
 
 // Do not edit below this line
