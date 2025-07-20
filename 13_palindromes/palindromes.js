603fd16ac672188271fbb18fb12d580bc12ddff4
eacosta <eacosta0411@gmail.com>
2025-07-19 20:05:32 -0400

    Passed all palindromes tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    filterOut = ['!', '.']
+    sentence = a.split('').filter( item => /[a-zA-Z1-9]/.test(item));
+    lowerCaseSentence = [];
+    sentence.forEach(letter => {
+        lowerCaseSentence.push(letter.toLowerCase());        
+    });
+    console.log(lowerCaseSentence);
+    if ( lowerCaseSentence.join('') == lowerCaseSentence.reverse().join('') ) {
+        return true;
+    } else {
+        return false;
+    };
 };
+
+
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
