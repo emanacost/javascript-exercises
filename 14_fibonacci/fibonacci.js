4ee3423d94f8acf6c3bf663df98561610c71626a
eacosta <eacosta0411@gmail.com>
2025-08-03 09:57:47 -0400

    Finished fibonacci section

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if ( n == "string") {
+        n = Number(n);
+    } else if ( n < 0) {
+        return "OOPS"
+    } else if (n == 0 ) {
+        return 0;
+    } else if (n == 1) {
+        return 1
+    };
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
